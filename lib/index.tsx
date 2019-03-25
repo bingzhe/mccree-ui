7676e7e2144af7d578225d33062dd23849156448
bingzhe <874177815@qq.com>
2019-03-25 21:27:35 +0800

    修改 依赖

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,1 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-// import Button from "./button/button";
-import Icon from "./icon/index";
-
-const fn: React.MouseEventHandler = (e) => {
-    console.log('icon click');
-}
-ReactDOM.render(
-    <div>
-        <Icon name="wechat" onClick={fn} />
-        <Icon name="alipay" onClick={fn} />
-        <Icon name="outline-delete-24px" onClick={fn} />
-    </ div>
-    , document.getElementById('root')
-);
+export { default as Icon } from "./icon/index";
