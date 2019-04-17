f87460c87a7f65157e60591615df49e2f1133772
bingzhe <874177815@qq.com>
2019-04-17 14:48:28 +0800

    添加 dialog 文件

--- site/example.tsx
+++ site/example.tsx
@@ -1,9 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import IconExample from '../lib/icon/demo/icon.example';
 import ButtonExample from '../lib/button/demo/button.example';
+import DialogExample from '../lib/dialog/demo/dialog.example';
 
 ReactDOM.render(
     <Router>
@@ -23,13 +23,16 @@ ReactDOM.render(
                         <li>
                             <Link to="/button">Button</Link>
                         </li>
+                        <li>
+                            <Link to="/dialog">Dialog</Link>
+                        </li>
                     </ul>
                 </aside>
                 <main>
-                    <Route path="/icon" component={IconExample} />
-                    <Route path="/button" component={ButtonExample} />
+                    <Route path="/icon" component={IconExample}/>
+                    <Route path="/button" component={ButtonExample}/>
+                    <Route path="/dialog" component={DialogExample}/>
                 </main>
             </div>
         </div>
-    </Router>
-    , document.querySelector('#root'));
+    </Router>, document.querySelector('#root'));
