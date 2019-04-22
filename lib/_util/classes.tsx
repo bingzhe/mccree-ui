d9c26a087aec197b9867fad174d289d982369871
bingzhe <874177815@qq.com>
2019-04-22 23:39:48 +0800

    添加 scopeClass

--- lib/_util/classes.tsx
+++ lib/_util/classes.tsx
@@ -2,4 +2,11 @@ function classes(...names: (string | undefined)[]) {
     return names.filter(Boolean).join(' ');
 }
 
+function scopeClassMaker(prefix: string) {
+    return function (name?: string) {
+        return [prefix, name].filter(Boolean).join("-");
+    }
+}
+
+export { scopeClassMaker };
 export default classes;
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 import "./dialog.scss";
+import { scopeClassMaker } from "../_util/classes";
+
+const scopeClass = scopeClassMaker('r-dialog');
+const sc = scopeClass;
 
 interface Props {
     visible: boolean
@@ -9,9 +13,17 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     return (
         props.visible ?
             <React.Fragment>
-                <div className="r-dialog-mask"></div>
-                <div className="r-dialog">
-                    <div>{props.children}</div>
+                <div className={sc('mask')}></div>
+                <div className={sc()}>
+                    <div className={sc('clsoe')}>X</div>
+                    <header className={sc('header')}></header>
+                    <main className={sc('main')}>
+                        {props.children}
+                    </main>
+                    <footer className={sc('footer')}>
+                        <button>ok</button>
+                        <button>cancel</button>
+                    </footer>
                 </div>
             </React.Fragment>
             :
