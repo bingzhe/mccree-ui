94b75b0aa6cc9bb07d4958f68f021548c81c6cfa
bingzhe <874177815@qq.com>
2019-04-23 14:07:03 +0800

    公告scss

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./dialog.scss";
 import { scopeClassMaker } from "../_util/classes";
 
-const scopeClass = scopeClassMaker('r-dialog');
+const scopeClass = scopeClassMaker('rui-dialog');
 const sc = scopeClass;
 
 interface Props {
