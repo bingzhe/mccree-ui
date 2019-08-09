f83d73555829170e96ff861d60806b6b5fba52f8
bingzhe <874177815@qq.com>
2019-08-09 21:24:20 +0800

    updata ci config

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/env bash
+
+# major: 主版本号
+# premajor: 预备主版本
+# minor: 次版本号
+# preminor: 预备次版本
+# patch: 修订号
+# prepatch: 预备修订版
+# prerelease: 预发布版本
+
 npm version $1 && \
     git push
--- site/play.tsx
+++ site/play.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import Code from "./Doc/Code";
 
 const textExample: React.FunctionComponent = () => {
-    const code = "<Button>Button  test</Button>";
+    const code = "<Button>Button  7777777</Button>";
 
     return (
         <div>
