f2d9bab6e63cd34b4f7373485047312754d3ea8d
bingzhe <874177815@qq.com>
2019-08-09 20:26:02 +0800

    site:doc init

--- lib/icon/importIcons.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import RequireContext = __WebpackModuleApi.RequireContext;
-
-let importAll = (requireContext: RequireContext) => requireContext.keys().forEach(requireContext);
-
-try {
-    importAll(require.context('../icons/', true, /\.svg$/));
-} catch (error) {
-}
--- lib/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Icon } from "./icon/index";
-export { default as Button } from "./button/index";
-export { default as Layout } from "./layout/index";
-
-import "./index.scss";
