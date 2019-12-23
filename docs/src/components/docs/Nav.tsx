c6d220c0228243a743cc5e4cbd012dfef8211954
bingzhe <874177815@qq.com>
2019-12-23 15:26:36 +0800

    docs(nav.tsx): change nav

--- docs/src/components/docs/Nav.tsx
+++ docs/src/components/docs/Nav.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { Icon, Navigation, Item } from "../../../../components/index";
+import { navigate } from "gatsby";
+
+import { Icon, Navigation, Item, ItemGroup } from "../../../../components/index";
 import { TemplateProps } from "./template";
 
 const iconMap = [
@@ -14,6 +16,10 @@ const iconMap = [
     },
 ];
 
+interface Acc {
+    [key: string]: string[];
+}
+
 interface Elem {
     type: string;
     title: string;
@@ -24,10 +30,27 @@ interface Result {
     titles: string[];
 }
 
-const getFrontMatter = (target: TemplateProps["data"]) => {
+const getFrontMatter = (target: TemplateProps["data"]): Result[] => {
     const classify = target.docs.edges
-        .map(v => v.node.frontmatter);
-    console.log("classify", classify);
+        .map((v) => v.node.frontmatter)
+        .reduce((acc, elem): Acc => {
+            const { type, title } = elem;
+            if (acc[type]) {
+                return {
+                    ...acc,
+                    [type]: [...acc[type], title]
+                };
+            }
+            return {
+                ...acc,
+                [type]: [title]
+            };
+        }, {});
+
+    return Object.keys(classify).map(type => ({
+        type,
+        titles: classify[type]
+    }));
 };
 
 const Nav: React.FC<TemplateProps> = ({ data }) => {
@@ -43,20 +66,45 @@ const Nav: React.FC<TemplateProps> = ({ data }) => {
         setDrawerVisible((e): boolean => !e);
     }, []);
 
-
+    const handleNavigation = React.useCallback((title: string, type: string): void => {
+        type === "hooks" ? navigate(`/hooks/${title}`) : navigate(`/components/${title.toLowerCase()}`);
+    }, []);
 
     const result = getFrontMatter(data);
+    console.log("result", result);
+    const rootRef = React.useRef<HTMLDivElement>(null);
 
     const PcChild = (
         <Navigation
+            ref={rootRef}
             value={activeId}
             expanded={expanded}
             acrylic
             height="100%"
+            width={260}
         >
             <Navigation.Header>
                 <Item onClick={handleExpanded} prefix={<Icon name="wechat" />}>123</Item>
             </Navigation.Header>
+            {result.map(
+                ({ type, titles }) => {
+                    return (
+                        <ItemGroup key={type} title={type}>
+                            {titles.map(
+                                title => (
+                                    <Item
+                                        key={title}
+                                        value={title}
+                                        onClick={handleNavigation.bind(undefined, title, type)}
+                                    >
+                                        {title}
+                                    </Item>
+                                )
+                            )}
+                        </ItemGroup>
+                    );
+                }
+            )}
         </Navigation>
     );
 
---
+++ docs/src/docs/components/Input/api.md
@@ -0,0 +1,13 @@
+---
+title: Input
+components: Input
+api: true
+---
+
+## API
+
+```
+import Box from '@fluent-ui/core/Box'
+// or
+import { Box } from '@fluent-ui/core'
+```
