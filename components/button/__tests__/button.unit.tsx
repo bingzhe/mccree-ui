e678c98e7e448c319c861b13fdedbde380fc64d8
bingzhe <874177815@qq.com>
2020-05-12 18:01:33 +0800

    test(button.unit.tsx): add a button test

--- components/button/__tests__/button.unit.tsx
+++ components/button/__tests__/button.unit.tsx
@@ -1,10 +1,14 @@
-import * as renderer from "react-test-renderer";
+// import * as renderer from "react-test-renderer";
 import * as React from "react";
 import Button from "../button";
+import { render } from "enzyme";
+import toJson from "enzyme-to-json";
 
 describe("button", () => {
-    it("是个 div", () => {
-        const json = renderer.create(<Button />).toJSON();
-        expect(json).toMatchSnapshot();
+    it("render <Button/> component", () => {
+        // const json = renderer.create(<Button />).toJSON();
+        const wrapper = render(<Button>Default</Button>);
+
+        expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
