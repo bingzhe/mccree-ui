a64add6e79e380883e3d8323567fa0a0ba1077a2
bingzhe <874177815@qq.com>
2020-05-13 14:49:19 +0800

    refactor(button.unit.tsx): add button unit test and remove react-test-renderer packages

--- components/button/__tests__/button.unit.tsx
+++ components/button/__tests__/button.unit.tsx
@@ -1,13 +1,25 @@
-// import * as renderer from "react-test-renderer";
 import * as React from "react";
-import Button from "../button";
 import { render } from "enzyme";
 import toJson from "enzyme-to-json";
 
+import Button from "../button";
+
 describe("button", () => {
     it("render <Button/> component", () => {
-        // const json = renderer.create(<Button />).toJSON();
-        const wrapper = render(<Button>Default</Button>);
+        const wrapper = render(
+            <>
+                <Button>DEFAULT</Button>
+                <Button type="primary">PRIMARY</Button>
+                <Button type="secondary">SECONDARY</Button>
+                <Button type="success" >SUCCESS</Button>
+                <Button type="error" >ERROE</Button>
+                <Button type="warning" >WARNING</Button>
+                <Button type="info" >INFO</Button>
+                <Button disabled>DISABLED</Button>
+                <Button target="block" href="http://bingzhe.github.io/" type="primary">LINK</Button>
+            </>
+        );
+
 
         expect(toJson(wrapper)).toMatchSnapshot();
     });
