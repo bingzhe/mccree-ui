61105ba7d24f0a015d94442c516c6b66f669f072
bingzhe <874177815@qq.com>
2020-05-27 15:21:04 +0800

    build(eslint): del local eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,6 @@
 module.exports = {
     parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-    extends: [
-        // 'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
-        // 'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
-        // './eslint/react.js',
-        // './eslint/typescript.js',
-        "alloy",
-        "alloy/react",
-        "alloy/typescript"
-    ],
+    extends: ["alloy", "alloy/react", "alloy/typescript"],
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: "module", // Allows for the use of imports
