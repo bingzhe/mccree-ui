79cf6e43fcfab4a626403deb83f8eb6105fa9bf0
bingzhe <874177815@qq.com>
2020-06-15 16:28:06 +0800

    build(gulpfile): bulid pref

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -337,6 +337,13 @@ async function pushPhPages(done) {
     done(0);
 }
 function moveDeployFile() {
+    rimraf.sync("./*.js");
+    rimraf.sync("./*.js.map");
+    rimraf.sync("./*.html");
+    rimraf.sync("./*.ico");
+    rimraf.sync("./*.LICENSE.txt");
+    rimraf.sync("./sb_dll");
+
     const move = gulp.src(["../storybook-static/**/*"]).pipe(gulp.dest("../"));
     return move;
 }
