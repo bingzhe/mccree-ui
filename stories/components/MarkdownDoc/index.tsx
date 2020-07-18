af69dbfb497c2df5ead6f12aa3708501209d9e31
bingzhe <874177815@qq.com>
2020-07-18 17:42:44 +0800

    refactor(markdowndoc): init markdown doc

---
+++ stories/components/MarkdownDoc/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface MarkdownDocProps {
+    className?: string;
+    demos?: any;
+    docs?: any;
+}
+
+const MarkdownDoc = () => {
+    return <div>MarkdownDoc</div>;
+};
+
+export default MarkdownDoc;
