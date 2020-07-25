c98c61823b1d7e3693fc1725c90cec7ce7c22946
bingzhe <874177815@qq.com>
2020-07-25 23:26:37 +0800

    refactor(input): input

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -1,7 +1,27 @@
 import * as React from "react";
+import classNames from "classnames";
 
-const Input = () => {
-    return <div>Input</div>
+import { ConfigContext } from "../config-provider";
+
+export interface InputProps {
+    className?: string;
+    disabled?: boolean;
+    defaultValue?: any;
+}
+
+const Input: React.FC<InputProps> = (props) => {
+    const { getPrefixCls } = React.useContext(ConfigContext);
+    const prefixCls = getPrefixCls("input");
+
+    const classes = classNames(prefixCls);
+
+    console.log({ classes });
+
+    return (
+        <div>
+            <input></input>
+        </div>
+    );
 };
 
 export default Input;
