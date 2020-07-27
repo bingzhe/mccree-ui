6b92d2cc64d5686124215fdffe73e74405d3e1c5
bingzhe <874177815@qq.com>
2020-07-27 17:47:07 +0800

    refactor(input): inptu style

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -7,19 +7,31 @@ export interface InputProps {
     className?: string;
     disabled?: boolean;
     defaultValue?: any;
+    value?: any;
+    addonBefore?: React.ReactNode;
+    addonAfter?: React.ReactNode;
+    prefix?: React.ReactNode;
+    suffix?: React.ReactNode;
+    variant?: "standard" | "outlined" | "filled";
 }
 
 const Input: React.FC<InputProps> = (props) => {
+    const { className, variant = "standard" } = props;
+
     const { getPrefixCls } = React.useContext(ConfigContext);
     const prefixCls = getPrefixCls("input");
 
-    const classes = classNames(prefixCls);
+    const inputRootClasses = classNames(`${prefixCls}-root`, {
+        [`${prefixCls}-outlined`]: variant === "outlined"
+    });
+    const inputClasses = classNames(prefixCls, className);
 
-    console.log({ classes });
+    console.log({ inputRootClasses });
+    console.log({ inputClasses });
 
     return (
-        <div>
-            <input></input>
+        <div className={inputRootClasses}>
+            <input className={inputClasses} placeholder="123" />
         </div>
     );
 };
