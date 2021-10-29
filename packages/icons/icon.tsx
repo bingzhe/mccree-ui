f236c231923e702c6669401bcfc27f4de7aa16bf
bingzhe <874177815@qq.com>
2021-10-29 17:45:55 +0800

    icons

--- packages/icons/icon.tsx
+++ packages/icons/icon.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import classNames from "classnames";
-import "./importIcons";
+// import "./importIcons";
 import { IconContext } from "./Context";
 import { useInsertStyles } from "./iconStyle";
 
