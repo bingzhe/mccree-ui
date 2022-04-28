7676cdd9deebf9bb67d8fb9a575379ff35b6f0d6
bingzhe <874177815@qq.com>
2022-04-28 11:14:56 +0800

    fix(icons) del bundle files

--- docs/mdx/Space/demo1.tsx
+++ docs/mdx/Space/demo1.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Space } from "mccree-ui/components";
 import "@mccree-ui/components/Space/style/index";
+import { IconAlipay, IconCaretDown, IconBackward } from "@mccree-ui/icons";
 
 export default () => {
     return (
@@ -9,6 +10,10 @@ export default () => {
             <div style={{ width: "50px", height: "50px", border: "1px solid #ccc" }}>2</div>
             <div style={{ width: "50px", height: "50px", border: "1px solid #ccc" }}>3</div>
             <div style={{ width: "50px", height: "50px", border: "1px solid #ccc" }}>4</div>
+
+            <IconAlipay style={{ height: "30px", width: "30px" }} />
+            <IconCaretDown style={{ height: "30px", width: "30px" }} />
+            <IconBackward style={{ height: "30px", width: "30px" }} />
         </Space>
     );
 };
