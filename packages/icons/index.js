866ecc3a3d25526a807f567955827818a58c7586
bingzhe <874177815@qq.com>
2022-05-27 23:37:05 +0800

    feat(svg) add svgs

--- packages/icons/index.js
+++ packages/icons/index.js
@@ -5,27 +5,467 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "IconAlipay", {
-  enumerable: true,
-  get: function get() {
-    return _index3["default"];
-  }
-});
-Object.defineProperty(exports, "IconBackward", {
+Object.defineProperty(exports, "IconArrowDown", {
   enumerable: true,
   get: function get() {
     return _index["default"];
   }
 });
-Object.defineProperty(exports, "IconCaretDown", {
+Object.defineProperty(exports, "IconArrowFall", {
   enumerable: true,
   get: function get() {
     return _index2["default"];
   }
 });
+Object.defineProperty(exports, "IconArrowLeft", {
+  enumerable: true,
+  get: function get() {
+    return _index3["default"];
+  }
+});
+Object.defineProperty(exports, "IconArrowRight", {
+  enumerable: true,
+  get: function get() {
+    return _index4["default"];
+  }
+});
+Object.defineProperty(exports, "IconArrowRise", {
+  enumerable: true,
+  get: function get() {
+    return _index5["default"];
+  }
+});
+Object.defineProperty(exports, "IconArrowUp", {
+  enumerable: true,
+  get: function get() {
+    return _index6["default"];
+  }
+});
+Object.defineProperty(exports, "IconCaretDown", {
+  enumerable: true,
+  get: function get() {
+    return _index7["default"];
+  }
+});
+Object.defineProperty(exports, "IconCaretLeft", {
+  enumerable: true,
+  get: function get() {
+    return _index8["default"];
+  }
+});
+Object.defineProperty(exports, "IconCaretRight", {
+  enumerable: true,
+  get: function get() {
+    return _index9["default"];
+  }
+});
+Object.defineProperty(exports, "IconCaretUp", {
+  enumerable: true,
+  get: function get() {
+    return _index10["default"];
+  }
+});
+Object.defineProperty(exports, "IconCheck", {
+  enumerable: true,
+  get: function get() {
+    return _index44["default"];
+  }
+});
+Object.defineProperty(exports, "IconCheckCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index42["default"];
+  }
+});
+Object.defineProperty(exports, "IconCheckCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index35["default"];
+  }
+});
+Object.defineProperty(exports, "IconCheckSquare", {
+  enumerable: true,
+  get: function get() {
+    return _index43["default"];
+  }
+});
+Object.defineProperty(exports, "IconClockCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index45["default"];
+  }
+});
+Object.defineProperty(exports, "IconClose", {
+  enumerable: true,
+  get: function get() {
+    return _index47["default"];
+  }
+});
+Object.defineProperty(exports, "IconCloseCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index46["default"];
+  }
+});
+Object.defineProperty(exports, "IconCloseCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index36["default"];
+  }
+});
+Object.defineProperty(exports, "IconDoubleDown", {
+  enumerable: true,
+  get: function get() {
+    return _index11["default"];
+  }
+});
+Object.defineProperty(exports, "IconDoubleLeft", {
+  enumerable: true,
+  get: function get() {
+    return _index12["default"];
+  }
+});
+Object.defineProperty(exports, "IconDoubleRight", {
+  enumerable: true,
+  get: function get() {
+    return _index13["default"];
+  }
+});
+Object.defineProperty(exports, "IconDoubleUp", {
+  enumerable: true,
+  get: function get() {
+    return _index14["default"];
+  }
+});
+Object.defineProperty(exports, "IconDown", {
+  enumerable: true,
+  get: function get() {
+    return _index16["default"];
+  }
+});
+Object.defineProperty(exports, "IconDownCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index15["default"];
+  }
+});
+Object.defineProperty(exports, "IconDragArrow", {
+  enumerable: true,
+  get: function get() {
+    return _index17["default"];
+  }
+});
+Object.defineProperty(exports, "IconExclamation", {
+  enumerable: true,
+  get: function get() {
+    return _index49["default"];
+  }
+});
+Object.defineProperty(exports, "IconExclamationCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index48["default"];
+  }
+});
+Object.defineProperty(exports, "IconExclamationCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index37["default"];
+  }
+});
+Object.defineProperty(exports, "IconExpand", {
+  enumerable: true,
+  get: function get() {
+    return _index18["default"];
+  }
+});
+Object.defineProperty(exports, "IconInfo", {
+  enumerable: true,
+  get: function get() {
+    return _index51["default"];
+  }
+});
+Object.defineProperty(exports, "IconInfoCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index50["default"];
+  }
+});
+Object.defineProperty(exports, "IconInfoCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index38["default"];
+  }
+});
+Object.defineProperty(exports, "IconLeft", {
+  enumerable: true,
+  get: function get() {
+    return _index20["default"];
+  }
+});
+Object.defineProperty(exports, "IconLeftCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index19["default"];
+  }
+});
+Object.defineProperty(exports, "IconMenuFold", {
+  enumerable: true,
+  get: function get() {
+    return _index21["default"];
+  }
+});
+Object.defineProperty(exports, "IconMenuUnfold", {
+  enumerable: true,
+  get: function get() {
+    return _index22["default"];
+  }
+});
+Object.defineProperty(exports, "IconMinus", {
+  enumerable: true,
+  get: function get() {
+    return _index53["default"];
+  }
+});
+Object.defineProperty(exports, "IconMinusCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index52["default"];
+  }
+});
+Object.defineProperty(exports, "IconMinusCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index39["default"];
+  }
+});
+Object.defineProperty(exports, "IconPlus", {
+  enumerable: true,
+  get: function get() {
+    return _index55["default"];
+  }
+});
+Object.defineProperty(exports, "IconPlusCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index54["default"];
+  }
+});
+Object.defineProperty(exports, "IconPlusCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index40["default"];
+  }
+});
+Object.defineProperty(exports, "IconQuestion", {
+  enumerable: true,
+  get: function get() {
+    return _index57["default"];
+  }
+});
+Object.defineProperty(exports, "IconQuestionCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index56["default"];
+  }
+});
+Object.defineProperty(exports, "IconQuestionCircleFill", {
+  enumerable: true,
+  get: function get() {
+    return _index41["default"];
+  }
+});
+Object.defineProperty(exports, "IconRight", {
+  enumerable: true,
+  get: function get() {
+    return _index24["default"];
+  }
+});
+Object.defineProperty(exports, "IconRightCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index23["default"];
+  }
+});
+Object.defineProperty(exports, "IconRotateLeft", {
+  enumerable: true,
+  get: function get() {
+    return _index25["default"];
+  }
+});
+Object.defineProperty(exports, "IconRotateRight", {
+  enumerable: true,
+  get: function get() {
+    return _index26["default"];
+  }
+});
+Object.defineProperty(exports, "IconShrink", {
+  enumerable: true,
+  get: function get() {
+    return _index27["default"];
+  }
+});
+Object.defineProperty(exports, "IconStop", {
+  enumerable: true,
+  get: function get() {
+    return _index58["default"];
+  }
+});
+Object.defineProperty(exports, "IconSwap", {
+  enumerable: true,
+  get: function get() {
+    return _index28["default"];
+  }
+});
+Object.defineProperty(exports, "IconToBottom", {
+  enumerable: true,
+  get: function get() {
+    return _index29["default"];
+  }
+});
+Object.defineProperty(exports, "IconToLeft", {
+  enumerable: true,
+  get: function get() {
+    return _index30["default"];
+  }
+});
+Object.defineProperty(exports, "IconToRight", {
+  enumerable: true,
+  get: function get() {
+    return _index31["default"];
+  }
+});
+Object.defineProperty(exports, "IconToTop", {
+  enumerable: true,
+  get: function get() {
+    return _index32["default"];
+  }
+});
+Object.defineProperty(exports, "IconUp", {
+  enumerable: true,
+  get: function get() {
+    return _index34["default"];
+  }
+});
+Object.defineProperty(exports, "IconUpCircle", {
+  enumerable: true,
+  get: function get() {
+    return _index33["default"];
+  }
+});
 
-var _index = _interopRequireDefault(require("./react-icon-cjs/IconBackward/index"));
+var _index = _interopRequireDefault(require("./react-icon-cjs/IconArrowDown/index"));
 
-var _index2 = _interopRequireDefault(require("./react-icon-cjs/IconCaretDown/index"));
+var _index2 = _interopRequireDefault(require("./react-icon-cjs/IconArrowFall/index"));
 
-var _index3 = _interopRequireDefault(require("./react-icon-cjs/IconAlipay/index"));
+var _index3 = _interopRequireDefault(require("./react-icon-cjs/IconArrowLeft/index"));
+
+var _index4 = _interopRequireDefault(require("./react-icon-cjs/IconArrowRight/index"));
+
+var _index5 = _interopRequireDefault(require("./react-icon-cjs/IconArrowRise/index"));
+
+var _index6 = _interopRequireDefault(require("./react-icon-cjs/IconArrowUp/index"));
+
+var _index7 = _interopRequireDefault(require("./react-icon-cjs/IconCaretDown/index"));
+
+var _index8 = _interopRequireDefault(require("./react-icon-cjs/IconCaretLeft/index"));
+
+var _index9 = _interopRequireDefault(require("./react-icon-cjs/IconCaretRight/index"));
+
+var _index10 = _interopRequireDefault(require("./react-icon-cjs/IconCaretUp/index"));
+
+var _index11 = _interopRequireDefault(require("./react-icon-cjs/IconDoubleDown/index"));
+
+var _index12 = _interopRequireDefault(require("./react-icon-cjs/IconDoubleLeft/index"));
+
+var _index13 = _interopRequireDefault(require("./react-icon-cjs/IconDoubleRight/index"));
+
+var _index14 = _interopRequireDefault(require("./react-icon-cjs/IconDoubleUp/index"));
+
+var _index15 = _interopRequireDefault(require("./react-icon-cjs/IconDownCircle/index"));
+
+var _index16 = _interopRequireDefault(require("./react-icon-cjs/IconDown/index"));
+
+var _index17 = _interopRequireDefault(require("./react-icon-cjs/IconDragArrow/index"));
+
+var _index18 = _interopRequireDefault(require("./react-icon-cjs/IconExpand/index"));
+
+var _index19 = _interopRequireDefault(require("./react-icon-cjs/IconLeftCircle/index"));
+
+var _index20 = _interopRequireDefault(require("./react-icon-cjs/IconLeft/index"));
+
+var _index21 = _interopRequireDefault(require("./react-icon-cjs/IconMenuFold/index"));
+
+var _index22 = _interopRequireDefault(require("./react-icon-cjs/IconMenuUnfold/index"));
+
+var _index23 = _interopRequireDefault(require("./react-icon-cjs/IconRightCircle/index"));
+
+var _index24 = _interopRequireDefault(require("./react-icon-cjs/IconRight/index"));
+
+var _index25 = _interopRequireDefault(require("./react-icon-cjs/IconRotateLeft/index"));
+
+var _index26 = _interopRequireDefault(require("./react-icon-cjs/IconRotateRight/index"));
+
+var _index27 = _interopRequireDefault(require("./react-icon-cjs/IconShrink/index"));
+
+var _index28 = _interopRequireDefault(require("./react-icon-cjs/IconSwap/index"));
+
+var _index29 = _interopRequireDefault(require("./react-icon-cjs/IconToBottom/index"));
+
+var _index30 = _interopRequireDefault(require("./react-icon-cjs/IconToLeft/index"));
+
+var _index31 = _interopRequireDefault(require("./react-icon-cjs/IconToRight/index"));
+
+var _index32 = _interopRequireDefault(require("./react-icon-cjs/IconToTop/index"));
+
+var _index33 = _interopRequireDefault(require("./react-icon-cjs/IconUpCircle/index"));
+
+var _index34 = _interopRequireDefault(require("./react-icon-cjs/IconUp/index"));
+
+var _index35 = _interopRequireDefault(require("./react-icon-cjs/IconCheckCircleFill/index"));
+
+var _index36 = _interopRequireDefault(require("./react-icon-cjs/IconCloseCircleFill/index"));
+
+var _index37 = _interopRequireDefault(require("./react-icon-cjs/IconExclamationCircleFill/index"));
+
+var _index38 = _interopRequireDefault(require("./react-icon-cjs/IconInfoCircleFill/index"));
+
+var _index39 = _interopRequireDefault(require("./react-icon-cjs/IconMinusCircleFill/index"));
+
+var _index40 = _interopRequireDefault(require("./react-icon-cjs/IconPlusCircleFill/index"));
+
+var _index41 = _interopRequireDefault(require("./react-icon-cjs/IconQuestionCircleFill/index"));
+
+var _index42 = _interopRequireDefault(require("./react-icon-cjs/IconCheckCircle/index"));
+
+var _index43 = _interopRequireDefault(require("./react-icon-cjs/IconCheckSquare/index"));
+
+var _index44 = _interopRequireDefault(require("./react-icon-cjs/IconCheck/index"));
+
+var _index45 = _interopRequireDefault(require("./react-icon-cjs/IconClockCircle/index"));
+
+var _index46 = _interopRequireDefault(require("./react-icon-cjs/IconCloseCircle/index"));
+
+var _index47 = _interopRequireDefault(require("./react-icon-cjs/IconClose/index"));
+
+var _index48 = _interopRequireDefault(require("./react-icon-cjs/IconExclamationCircle/index"));
+
+var _index49 = _interopRequireDefault(require("./react-icon-cjs/IconExclamation/index"));
+
+var _index50 = _interopRequireDefault(require("./react-icon-cjs/IconInfoCircle/index"));
+
+var _index51 = _interopRequireDefault(require("./react-icon-cjs/IconInfo/index"));
+
+var _index52 = _interopRequireDefault(require("./react-icon-cjs/IconMinusCircle/index"));
+
+var _index53 = _interopRequireDefault(require("./react-icon-cjs/IconMinus/index"));
+
+var _index54 = _interopRequireDefault(require("./react-icon-cjs/IconPlusCircle/index"));
+
+var _index55 = _interopRequireDefault(require("./react-icon-cjs/IconPlus/index"));
+
+var _index56 = _interopRequireDefault(require("./react-icon-cjs/IconQuestionCircle/index"));
+
+var _index57 = _interopRequireDefault(require("./react-icon-cjs/IconQuestion/index"));
+
+var _index58 = _interopRequireDefault(require("./react-icon-cjs/IconStop/index"));
