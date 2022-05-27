9413498bc9778f00d73335094537352515ce5366
bingzhe <874177815@qq.com>
2022-05-27 23:17:28 +0800

    fix(svg) delete icon

--- docs/mdx/Space/demo1.tsx
+++ docs/mdx/Space/demo1.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Space } from "@mccree-ui/components";
 import "@mccree-ui/components/Space/style/index";
-import { IconAlipay, IconCaretDown, IconBackward } from "@mccree-ui/icons";
+// import { IconAlipay, IconCaretDown, IconBackward } from "@mccree-ui/icons";
 
 export default () => {
     return (
@@ -11,9 +11,9 @@ export default () => {
             <div style={{ width: "50px", height: "50px", border: "1px solid #ccc" }}>3</div>
             <div style={{ width: "50px", height: "50px", border: "1px solid #ccc" }}>4</div>
 
-            <IconAlipay style={{ height: "30px", width: "30px" }} />
+            {/* <IconAlipay style={{ height: "30px", width: "30px" }} />
             <IconCaretDown style={{ height: "30px", width: "30px" }} />
-            <IconBackward style={{ height: "30px", width: "30px" }} />
+            <IconBackward style={{ height: "30px", width: "30px" }} /> */}
         </Space>
     );
 };
